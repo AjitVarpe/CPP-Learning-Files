f66704aadcf289565dcdbb39d856aaefefc0a81f
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-03-21 09:05:40 +0530

    string function files added

---
+++ Stringsfn.cpp
@@ -0,0 +1,33 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    string ajit;
+    // TO get single word input from given string.
+    // cin>>ajit;
+
+    getline(cin,ajit);  // To get whole line of input.
+    cout<<ajit<<endl;   
+   
+
+    //  String Function 
+    // Asssign value to String
+    // 1.append
+    // 2.str1 + str2
+    // Access perticular element by giving index str1[1]
+    // .clear
+    // s1.compare(s2) return 0 when string are same
+    // s1.empty() return true when string is empty
+    // s1.erase(start,how much element);
+    // s1.find("ajit") return the element from where string start
+    // s1.insert(where index,"string");
+    // s1.length()/size(); return length
+    // s2 = s1.substr(from index ,number of elementsss)
+    // int x = stoi(s1);convert str number to integer type.
+    // to_string(s1) int to string
+    // sort (s1.begin(),s1.end());
+
+    return 0;
+}
