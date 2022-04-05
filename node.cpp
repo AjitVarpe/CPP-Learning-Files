7b3a78176d0731ec637c072455a5c6c027fc1f05
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-04-05 21:13:04 +0530

    init

--- node.cpp
+++ node.cpp
@@ -1,11 +1,13 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 #include <iostream>
 using namespace std;
 
-class Node {
+class Node
+{
+
 public:
 	int data;
-	Node* next;
+	Node *next;
 
 	Node()
 	{
@@ -20,35 +22,36 @@ public:
 	}
 };
 
+class Linkedlist
+{
+	Node *head;
 
-class Linkedlist {
-	Node* head;
-
-    public:
-
+public:
 	Linkedlist() { head = NULL; }
 
 	void insertNode(int);
 	void printList();
-    int totalnodes();
-    int additionofnode();
+	int totalnodes();
+	int additionofnode();
 };
 
 // Function to insert a new node.
 void Linkedlist::insertNode(int data)
 {
 	// Create the new Node.
-	Node* newNode = new Node(data);
+	Node *newNode = new Node(data);
 
 	// Assign to head
-	if (head == NULL) {
+	if (head == NULL)
+	{
 		head = newNode;
 		return;
 	}
 
 	// Traverse till end of list
-	Node* temp = head;
-	while (temp->next != NULL) {
+	Node *temp = head;
+	while (temp->next != NULL)
+	{
 
 		// Update temp
 		temp = temp->next;
@@ -61,16 +64,18 @@ void Linkedlist::insertNode(int data)
 // Function to print the nodes of the linked list.
 void Linkedlist::printList()
 {
-	Node* temp = head;
+	Node *temp = head;
 
 	// Check for empty list.
-	if (head == NULL) {
+	if (head == NULL)
+	{
 		cout << "List empty" << endl;
 		return;
 	}
 
 	// Traverse the list.
-	while (temp != NULL) {
+	while (temp != NULL)
+	{
 		cout << temp->data << " ";
 		temp = temp->next;
 	}
@@ -78,67 +83,72 @@ void Linkedlist::printList()
 // Function to print the total of the linked list.
 int Linkedlist::totalnodes()
 {
-    Node* temp = head;
-    int cnt=1;
-    if (head == NULL) {
+	Node *temp = head;
+	int cnt = 1;
+	if (head == NULL)
+	{
 		cout << "List empty" << endl;
-		
-	}else{
-    
-	while (temp->next != NULL) {
-        cnt++;
-		temp = temp->next;
-	}}
-    return cnt;
+	}
+	else
+	{
+
+		while (temp->next != NULL)
+		{
+			cnt++;
+			temp = temp->next;
+		}
+	}
+	return cnt;
 }
 
 // Function to print the total of the linked list.
 int Linkedlist::additionofnode()
 {
-    Node* temp = head;
-    int total=0;
-    if (head == NULL) {
-		total=0;
-		
-	}else{
-    
-	 while(temp->next != NULL){
-		total = temp->data + total;
-        temp = temp->next;
+	Node *temp = head;
+	int total = 0;
+	if (head == NULL)
+	{
+		total = 0;
 	}
-    }
-    total=temp->data + total;
-    return total;
+	else
+	{
+
+		while (temp->next != NULL)
+		{
+			total = temp->data + total;
+			temp = temp->next;
+		}
+	}
+	total = temp->data + total;
+	return total;
 }
 
 // Driver Code
 int main()
 {
 	Linkedlist list;
-    int count;
-    cout<<"How Many Nodes Do We Want To Enter : "<<endl;
-    cin>>count;
+	int count;
+	cout << "How Many Nodes Do We Want To Enter : " << endl;
+	cin >> count;
 
-    for (int i = 0; i < count; i++)
-    {
-        int Data;
-        cout<<"Enter Data : "<<endl;
-        cin>>Data;
-     	list.insertNode(Data);
-    }
+	for (int i = 0; i < count; i++)
+	{
+		int Data;
+		cout << "Enter Data : " << endl;
+		cin >> Data;
+		list.insertNode(Data);
+	}
 
 	cout << "Elements of the list are: ";
 	list.printList();
 	cout << endl;
-    cout<<"Total Number Of Nodes In Linklist are : "<<endl;
-    cout<<list.totalnodes();
+	cout << "Total Number Of Nodes In Linklist are : " << endl;
+	cout << list.totalnodes();
 
-    cout<<"Addition Of All Nodes In Linklist are : ";
-    cout<<list.additionofnode();
-    cout<<endl;
-    cout<<"Avg of All Nodes is : "<<list.additionofnode() / list.totalnodes();
-
-
+	cout << "Addition Of All Nodes In Linklist are : ";
+	cout << list.additionofnode();
+	cout << endl;
+	cout << "Avg of All Nodes is : " << list.additionofnode() / list.totalnodes();
 
 	return 0;
 }
