f0a91971a25b849b90a4803af32a69aaa13d16c7
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-04-11 12:44:06 +0530

    init commit

---
+++ Ajit.h
@@ -0,0 +1,12 @@
+int sum(int a,int b){
+    return (a+b);
+}
+int division(int a,int b){
+    return (a/b);
+}
+int multi(int a,int b){
+    return (a*b);
+}
+int sub(int a, int b){
+    return (a-b);
+}
---
+++ haederacces.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+#include "Ajit.h"
+using namespace std;
+
+int main(){
+    int a,b;
+    cout<<"Enter two Number "<<endl;
+    cin>>a>>b;
+    cout<<"Addition is : ";
+    cout<<sum(a,b);
+
+    
+
+
+    return 0;
+}
