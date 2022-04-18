013f5ae5fc6c70e9ee61f6ea2b4990b51e549996
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-04-18 18:08:08 +0530

    init

---
+++ Substring.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+
+std::string substring(const std::string,int a,int b)
+{
+    {
+    int a=0;
+    strjng newstr;
+        for(int i=s;i<=e;i++){
+        newstr[a]=str[i];
+        a++;
+        }    
+    return newstr;
+}
+}
+
+int main()
+{
+    std::string message;
+    getline(std::cin, message);
+    message = Xout(message);
+    std::cout << message << '\n';
+}
