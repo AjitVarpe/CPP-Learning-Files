ca96c6d89d0db3a5496d43d525f57c654751b3ae
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-04-19 16:01:59 +0530

    init

---
+++ doublylinklist.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+using namespace std;
+struct Node {
+   int data;
+   struct Node *prev;
+   struct Node *next;
+};
+struct Node* head = NULL;
+void insert(int newdata) {
+   struct Node* newnode = (struct Node*) malloc(sizeof(struct Node));
+   newnode->data = newdata;
+   newnode->prev = NULL;
+   newnode->next = head;
+   if(head != NULL)
+   head->prev = newnode ;
+   head = newnode;
+}
+void display() {
+   struct Node* ptr;
+   ptr = head;
+   while(ptr != NULL) {
+      cout<< ptr->data <<" ";
+      ptr = ptr->next;
+   }
+}
+int main() {
+    int n,m;
+    cout<<"Enter how many Element we Want to insert "<<endl;
+    cin>>n;
+    for(int i= 0; i<n;i++){
+        cout<<"Enter Element Data"<<endl;
+        cin>>m;
+        insert(m);
+    }
+   cout<<"The doubly linked list is: ";
+   display();
+   return 0;
+}
