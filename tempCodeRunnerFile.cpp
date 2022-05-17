952e4a1ee7db06fff5b61347615143a8f1f68ea7
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-05-17 16:42:20 +0530

    initt

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,28 +1,35 @@
-# include <bits/stdc++.h>
+#include <bits/stdc++.h>
+#include <limits>
+#include <String>
 using namespace std;
 
-void rotate(int arr[], int n)
-{
-	int x = arr[n - 1], i;
-	for (i = n - 1; i > 0; i--)
-	arr[i] = arr[i - 1];
-	arr[0] = x;
-}
 int main()
 {
-	int arr[] = {1, 2, 3, 4, 5}, i;
-	int n = sizeof(arr) /
-			sizeof(arr[0]);
-
-	cout << "Given array is \n";
-	for (i = 0; i < n; i++)
-		cout << arr[i] << ' ';
-
-	rotate(arr, n);
-
-	cout << "\nRotated array is\n";
-	for (i = 0; i < n; i++)
-		cout << arr[i] << ' ';
+	int n,temp=0;
+	cin>>n;
+	int t=n;
+	int arr[n];
+	cout<<"Enter Element : ";
+		for (int i = 0; i < n; i++)
+	{
+		cin>>arr[i];
+	}
+	for(int i=n;i>0;i--){
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i]  arr[i + 1])
+		{
+			temp = arr[i];
+			arr[i] = arr[i + 1];
+			arr[i+1] = temp;
+		}
+		
+	}
+	}
+	for (int i = 0; i < t; i++)
+	{
+		cout << arr[i]<<", ";
+	}
 
 	return 0;
 }
--- trail.cpp
+++ trail.cpp
@@ -1,27 +1,35 @@
-#include<bits/stdc++.h>
-#include<limits>
-#include<String>
+#include <bits/stdc++.h>
+#include <limits>
+#include <String>
 using namespace std;
 
-int main(){
-    int n;
-    cout<<"Enter Size of Array : ";
-    cin>>n;
-    int arr[n];
-    for(int i=0;i<n;i++){
-        cin>>arr[i];
-    }
+int main()
+{
+	int n, temp = 0;
+	cin >> n;
+	int t = n;
+	int arr[n];
+	cout << "Enter Element : ";
+	for (int i = 0; i < n; i++)
+	{
+		cin >> arr[i];
+	}
+	for (int i = 0; i < n-1; i++)
+	{
+		for (int j = 1; j < n; j++)
+		{
+			if (arr[i] > arr[j])
+			{
+				temp = arr[i];
+				arr[i] = arr[j];
+				arr[j] = temp;
+			}
+		}
+	}
+	for (int i = 0; i < t; i++)
+	{
+		cout << arr[i] << ", ";
+	}
 
-    int x=arr[n-1];
-    for(int i=n-1;i>0;i--)
-    {
-        arr[i]=arr[i-1];
-    }
-    arr[0]=x;
-    for(int i=0;i<n;i++){
-        cout<<arr[i]<<", ";
-    }
-
-
-    return 0;
+	return 0;
 }
