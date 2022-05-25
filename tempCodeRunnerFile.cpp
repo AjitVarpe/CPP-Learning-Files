62c2a599caac40769ac664b28332091668af9e11
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-05-25 09:51:17 +0530

    added Sorting Programs to the git file

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,35 +1,63 @@
-#include <bits/stdc++.h>
-#include <limits>
-#include <String>
+#include <iostream>
 using namespace std;
-
+void merge(int array[], int const left, int const mid, int const right)
+{
+auto const subArrayOne = mid - left + 1;
+auto const subArrayTwo = right - mid;
+auto *leftArray = new int[subArrayOne],
+*rightArray = new int[subArrayTwo];
+for (auto i = 0; i < subArrayOne; i++)
+leftArray[i] = array[left + i];
+for (auto j = 0; j < subArrayTwo; j++)
+rightArray[j] = array[mid + 1 + j];
+auto indexOfSubArrayOne = 0,
+indexOfSubArrayTwo = 0;
+int indexOfMergedArray = left;
+while (indexOfSubArrayOne < subArrayOne && indexOfSubArrayTwo <
+subArrayTwo) {
+if (leftArray[indexOfSubArrayOne] <= rightArray[indexOfSubArrayTwo]) {
+array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+indexOfSubArrayOne++;
+}
+else {
+array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+indexOfSubArrayTwo++;
+}
+indexOfMergedArray++;
+}
+while (indexOfSubArrayOne < subArrayOne) {
+array[indexOfMergedArray] = leftArray[indexOfSubArrayOne];
+indexOfSubArrayOne++;
+indexOfMergedArray++;
+}
+while (indexOfSubArrayTwo < subArrayTwo) {
+array[indexOfMergedArray] = rightArray[indexOfSubArrayTwo];
+indexOfSubArrayTwo++;
+indexOfMergedArray++;
+}
+}
+void mergeSort(int array[], int const begin, int const end)
+{
+if (begin >= end)
+return;
+auto mid = begin + (end - begin) / 2;
+mergeSort(array, begin, mid);
+mergeSort(array, mid + 1, end);
+merge(array, begin, mid, end);
+}
+void printArray(int A[], int size)
+{
+for (auto i = 0; i < size; i++)
+cout << A[i] << " ";
+}
 int main()
 {
-	int n,temp=0;
-	cin>>n;
-	int t=n;
-	int arr[n];
-	cout<<"Enter Element : ";
-		for (int i = 0; i < n; i++)
-	{
-		cin>>arr[i];
-	}
-	for(int i=n;i>0;i--){
-	for (int i = 0; i < n; i++)
-	{
-		if (arr[i]  arr[i + 1])
-		{
-			temp = arr[i];
-			arr[i] = arr[i + 1];
-			arr[i+1] = temp;
-		}
-		
-	}
-	}
-	for (int i = 0; i < t; i++)
-	{
-		cout << arr[i]<<", ";
-	}
-
-	return 0;
+int arr[] = { 12, 11, 13, 5, 6, 7 };
+auto arr_size = sizeof(arr) / sizeof(arr[0]);
+cout << "Given array is \n";
+printArray(arr, arr_size);
+mergeSort(arr, 0, arr_size - 1);
+cout << "\nSorted array is \n";
+printArray(arr, arr_size);
+return 0;
 }
--- trail.cpp
+++ trail.cpp
@@ -1,35 +1,24 @@
 #include <bits/stdc++.h>
-#include <limits>
-#include <String>
+#include<climits>
 using namespace std;
-
-int main()
-{
-	int n, temp = 0;
-	cin >> n;
-	int t = n;
+int main(){
+	int n;
+	cin>>n;
 	int arr[n];
-	cout << "Enter Element : ";
-	for (int i = 0; i < n; i++)
-	{
-		cin >> arr[i];
+	for(int i=0;i<n;i++){
+		cin>>arr[i];
 	}
-	for (int i = 0; i < n-1; i++)
-	{
-		for (int j = 1; j < n; j++)
-		{
-			if (arr[i] > arr[j])
-			{
-				temp = arr[i];
-				arr[i] = arr[j];
-				arr[j] = temp;
+	for(int i=0;i<n-1;i++){
+		for(int j=i+1;j<n;j++){
+			if(arr[j]<arr[i]){
+				int temp=arr[i];
+				arr[i]=arr[j];
+				arr[j]=temp;
 			}
 		}
 	}
-	for (int i = 0; i < t; i++)
-	{
-		cout << arr[i] << ", ";
+	for(int i=0;i<n;i++){
+		cout<<arr[i]<<", ";
 	}
-
 	return 0;
 }
