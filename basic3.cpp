05b236da8b53ad84d620a022bcfad4723498da6d
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-05-26 10:29:19 +0530

    init

---
+++ basic3.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    string s;
+    cout<<"Enter the String :";
+    getline(cin,s); 
+    // getline will take the the all line ofinput from user " Ajit Varpe" including space  
+    
+    // cin>>s; // Cin will take only upto Frist Space for ex: Ajit Varpe is the input then it will only take Ajit As a input
+
+
+    cout<<"Your Enterd string is :"<<endl;
+    cout<<s;
+
+
+
+    return 0;
+}
