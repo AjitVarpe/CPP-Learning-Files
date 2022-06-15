669d7bc6aa8297745361a13b00bbb98ddb3156df
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-06-15 10:30:53 +0530

    init

--- trail.cpp
+++ trail.cpp
@@ -1,25 +1,39 @@
-#include<bits/stdc++.h>
+ #include<bits/stdc++.h>
+#include<stdio.h>
 #include<limits>
-#include<String>
 using namespace std;
-class Node{
-	public:
-	int data;
-	Node* next;
-};
+
+//  Crerating Function for Returning the Index Of Required Element
+int binary(int arr[],int n,int key)
+{
+	int s=0,e=n;
+	while(s<=e){
+			int mid =(s+e)/2;
+		if(arr[mid]==key){
+			return mid;
+		}else if(arr[mid]>key){
+			e=mid-1;
+		}else{
+			s=mid+1;
+		}
+	}	
+	return -1;
+}
 int main(){
-	Node* head = new Node();
-	Node* second = new Node();
-	Node* third = new Node();
 
-	head->data = 5;
-	head->next = second;
-	second->data=6;
-	second->next=third;
-	third->data=7;
-	third->next=NULL;
-	cout<<second->next;
+	// Accepting Size of Array and Search key
+	int n,key;
+	cin>>n;
+	cin>>key;
 
 
-	return 0;
+	// Inserting Array Element 
+	int arr[n];
+	for (int i=0;i<n;i++){
+		cin>>arr[i];
+	}
+
+	// Filling Values in the Function
+	cout<<binary(arr,n,key);
+  return 0;
 }
