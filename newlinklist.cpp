3af565e470d5750fd3f8beee89d60d8c6a226290
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-06-16 18:54:55 +0530

    insert new code

--- newlinklist.cpp
+++ newlinklist.cpp
@@ -5,74 +5,45 @@ using namespace std;
 class Node{
     public:
     int data;
-    Node* next;
+    Node *next;
 };
 void printlist(Node* n){
-    while (n != NULL)
-    {
+    while(n != NULL){
         cout<<n->data<<" ";
         n = n->next;
     }
-    
 }
-// void insert_at_end(Node* head,int data){
-//     Node *temp,*ptr;
-//     ptr = head;
-//     temp = new Node();
-//     temp->data=data;
-//     temp->next=NULL;
-//     while(ptr->next != NULL){
-//         ptr = ptr->next;
-//     }
-//     ptr->next = temp; 
-// }
-// void printlist(Node* head){
-//     while(n != NULL){
-//         cout<<n->data<<" ";
-//         n = n->next;
-//     }
-// }
-
 void insert_at_end(Node *head,int data){
-    Node *ptr,*temp;
+    Node *temp,*ptr;
     ptr = head;
-    temp  = new Node();
-    temp->data=data;
-    temp->next=NULL;
-    if(ptr->next==NULL){
-        ptr->next=temp;
+    temp = new Node();
+    temp->data = data;
+    temp->next = NULL;
+    if(ptr->next == NULL){
+        ptr->next = temp;
     }
-    while(ptr->next != NULL){
+    while(ptr->next!=NULL){
         ptr = ptr->next;
     }
-    ptr->next= temp;
-    }
-
-
-
-
+    ptr->next=temp;
+}
 int main(){
-
-  Node* head = NULL;
-  Node* second = NULL;
-  Node* third = NULL;
+    Node *head = NULL;
+    Node *second = NULL;
+    Node *third = NULL;
 
     head = new Node();
     second = new Node();
     third = new Node();
 
-    head->data= 15;
-    head->next=second;
-    second->data= 25;
-    second->next=third;
-    third->data= 35;
-    third->next=NULL;
+    head->data=25;
+    head->next = second;
+    second->data=35;
+    second->next = third;
+    third->data=45;
+    third->next = NULL;
 
-    insert_at_end(head,45);
-
+    insert_at_end(head,55);
     printlist(head);
-
-
-
     return 0;
 }
