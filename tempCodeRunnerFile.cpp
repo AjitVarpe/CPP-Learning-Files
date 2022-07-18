9c8c34275d4147b4604dfff31283f2166a72f2f6
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-18 14:30:36 +0530

    added  pattern question

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,27 @@
- // root->data = 10;
-    // root->next = NULL;
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while (i<=n)
+    {
+        int j=i;
+        while (j<=n)
+        {
+            cout<<j;
+            j++;
+        }
+        cout<<endl;
+        i++;
+
+        
+    }
+    
+
+
+    return 0;
+}
