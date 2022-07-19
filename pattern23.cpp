0a19302a3e21130c94dfe95d7790f389dad63dfb
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-19 13:25:38 +0530

    Added new Patterns

--- pattern23.cpp
+++ pattern23.cpp
@@ -4,8 +4,26 @@
 using namespace std;
 
 int main(){
-    int n;
-    
+    int n,number=1;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int space = n-i;
+        while (space--){
+            cout<<"  ";
+        }
+        int col=1;
+        while (col<=i)
+        {
+            cout<<number++<<" ";
+            col++;
+        }
+        cout<<endl;
+        i++;
+
+        
+         
+    }
 
 
     return 0;
