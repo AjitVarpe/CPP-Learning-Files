53d00f8e234b28fb7638c76c35d7bcca569fa256
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-19 12:38:57 +0530

    inserted new patterns

--- pattern18.cpp
+++ pattern18.cpp
@@ -1,22 +1,26 @@
-#include<stdio.h>
-#include<iostream>
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
 using namespace std;
-int main(){
 
+int main(){
     int n;
     cin>>n;
     int i=1;
-    
     while(i<=n){
         int j=1;
-        char ch = 'A' + n - i;
+        char ch  = 'A'+ n - i;
         while (j<=i)
         {
             cout<<ch++<<" ";
             j++;
+
         }
         cout<<endl;
         i++;
         
     }
+
+
+    return 0;
 }
