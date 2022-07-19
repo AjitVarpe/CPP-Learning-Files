07d5b6a757284dc7a3b7cc22a953553a67c705e5
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-19 13:02:22 +0530

    added new patterns to repo

---
+++ pattern22.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    int i=1;
+    while(i<=n){
+        int space = i-1;
+        while(space--){cout<<" ";}
+        int star = n-i+1;
+        while(star--){
+            cout<<"*";
+        }
+        cout<<endl;
+        i++;
+        
+    }
+
+
+    return 0;
+}
