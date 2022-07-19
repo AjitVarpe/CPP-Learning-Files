098215ebb00b4c4091b0850718eb47ade4a688ca
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-19 12:27:28 +0530

    insert  new patterns questions

--- pattern14.cpp
+++ pattern14.cpp
@@ -6,11 +6,10 @@ int main(){
     int n;
     cin>>n;
     int i=1;
-    
+    char ch = 'A';
     while(i<=n){
         int j=1;
-        char ch = 'A' + n - i;
-        while (j<=i)
+        while (j<=n)
         {
             cout<<ch++<<" ";
             j++;
--- pattern15.cpp
+++ pattern15.cpp
@@ -6,10 +6,9 @@ int main(){
     int n;
     cin>>n;
     int i=1;
-    
+    char ch = 'A';
     while(i<=n){
         int j=1;
-        char ch = 'A' + n - i;
         while (j<=i)
         {
             cout<<ch++<<" ";
