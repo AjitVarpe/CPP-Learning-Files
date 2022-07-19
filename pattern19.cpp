1b091d945bd47b492d5dee62e55c6768077376e9
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-19 12:58:28 +0530

    inserted new patterns codes in the repo

--- pattern19.cpp
+++ pattern19.cpp
@@ -7,15 +7,21 @@ int main(){
     int n;
     cin>>n;
     int i=1;
-    while(i<=n){
-        int star = n-i+1;
-        while(star--){
-            cout<<"*";
+    while (i<=n)
+    {
+        int j=1;
+        char ch = 'A'+i-1;
+        while (j<=n)
+        {
+            cout<<ch++<<" ";
+            j++;
         }
         cout<<endl;
         i++;
         
+
     }
+    
 
 
     return 0;
