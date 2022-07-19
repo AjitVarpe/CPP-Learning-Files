6c15a2dee189a16b9f478b9b00d589e89d61cbf6
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-07-19 13:15:53 +0530

    commited file

--- pattern22.cpp
+++ pattern22.cpp
@@ -1,24 +1,28 @@
-#include<bits/stdc++.h>
-#include<limits>
-#include<String>
+#include <bits/stdc++.h>
+#include <limits>
+#include <String>
 using namespace std;
 
-int main(){
+int main()
+{
     int n;
-    cin>>n;
-    int i=1;
-    while(i<=n){
-        int space = i-1;
-        while(space--){cout<<" ";}
-        int star = n-i+1;
-        while(star--){
-            cout<<"*";
+    cin >> n;
+    int i = 1;
+    while (i <= n)
+    {
+        int space = i - 1;
+        while (space--)
+        {
+            cout << " ";
         }
-        cout<<endl;
+        int count =  i;
+        while (count<=n)
+        {
+            cout << count++;
+        }
+        cout << endl;
         i++;
-        
     }
-
 
     return 0;
 }
---
+++ pattern23.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+#include<limits>
+#include<String>
+using namespace std;
+
+int main(){
+    int n;
+    
+
+
+    return 0;
+}
