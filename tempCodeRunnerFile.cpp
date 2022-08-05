a40f65e09088131fc8dbc89816c12afc4ed9413c
Ajit Varpe <ajitvarpe8652@gmail.com>
2022-08-05 20:50:07 +0530

    insert new codes

--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,30 +1,10 @@
-#include<bits/stdc++.h>
-#include<limits>
-#include<String>
-using namespace std;
-
-int main(){
-    int n,number=1;
-    cin>>n;
-    int i=1;
-    while(i<=n){
-        int space = n-i;
-        while (space--){
-            cout<<" ";
-        }
-        int col=1;
-        while (col<=i)
-        {
-            cout<<number++<<" ";
-            col++;
-        }
-        cout<<endl;
-        i++;
-
-        
-         
-    }
-
-
-    return 0;
-}
+// CPP program to print 
+// ASCII Value of Character 
+#include <iostream> 
+using namespace std; 
+int main() 
+{ 
+	char c = 't'; 
+	cout << "The ASCII value of " << c << " is " << int(c); 
+	return 0; 
+} 
